f2e18bf958bd125030ba015977d23c22f2aa88b5
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2025-12-07 18:54:33 +0400

    Add reviews section with Swiper slider and styling
    - Introduced a new SVG icon for Yandex reviews.
    - Implemented a reviews slider using Swiper with keyboard navigation and scrollbar support.
    - Created SCSS styles for the reviews section, including responsive design and layout adjustments.
    - Enhanced review card functionality with expandable text and button interactions.

---
+++ src/js/libraries/libraries/swiper/sliders/reviews-slider.js
@@ -0,0 +1,76 @@
+import Swiper from "swiper";
+import { Keyboard, Navigation, Scrollbar, } from "swiper/modules";
+
+/** @type {NodeListOf<HTMLDivElement>} */
+const reviewsSliders = document.querySelectorAll(".reviews-slider");
+
+reviewsSliders.forEach((reviewsSlider) => {
+  /** @type {HTMLDivElement} */
+  const parent = reviewsSlider.closest(".reviews__slider");
+  /** @type {HTMLDivElement} */
+  const scrollbar = parent.querySelector(".compact-controllers__scrollbar");
+  /** @type {HTMLDivElement} */
+  const next = parent.querySelector(".compact-arrows__button--next");
+  /** @type {HTMLDivElement} */
+  const prev = parent.querySelector(".compact-arrows__button--prev");
+
+  const swiper = new Swiper(reviewsSlider, {
+    modules: [Keyboard, Navigation, Scrollbar,],
+    keyboard: {
+      enabled: true,
+      pageUpDown: false,
+    },
+    navigation: {
+      enabled: true,
+      nextEl: next,
+      prevEl: prev,
+    },
+    scrollbar: {
+      draggable: true,
+      el: scrollbar,
+      enabled: true,
+    },
+    breakpoints: {
+      "600.1": {
+        slidesPerView: 1,
+        spaceBetween: 12,
+      },
+    },
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    autoHeight: true,
+    rewind: true,
+  });
+
+  /** @type {NodeListOf<HTMLButtonElement>} */
+  const reviewsTextButtons = reviewsSlider.querySelectorAll(".review-card__button");
+
+  reviewsTextButtons.forEach((reviewsTextButton) => {
+    const { dataset } = reviewsTextButton;
+    const { showText = "Развернуть", hideText = "Свернуть" } = dataset;
+    /** @type {HTMLDivElement} */
+    const previous = reviewsTextButton.previousElementSibling;
+
+    const previousResizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        /** @type {{ target: HTMLDivElement, borderBoxSize: ResizeObserverSize[] }} */
+        const { target, borderBoxSize } = entry;
+        const { scrollHeight } = target;
+        const { blockSize } = borderBoxSize[0];
+
+        target.classList.toggle("review-card__inner--active", scrollHeight > blockSize);
+      });
+    });
+
+    previousResizeObserver.observe(previous);
+
+    if (previous && previous.classList.contains("review-card__inner")) {
+      reviewsTextButton.addEventListener("click", () => {
+        reviewsTextButton.classList.toggle("review-card__button--active");
+        previous.classList.toggle("review-card__inner--show");
+        reviewsTextButton.textContent = previous.classList.contains("review-card__inner--show") ? hideText : showText;
+        swiper.update();
+      });
+    }
+  });
+});
--- src/js/libraries/libraries/swiper/swiper.js
+++ src/js/libraries/libraries/swiper/swiper.js
@@ -1,3 +1,4 @@
 import "./sliders/list-slider.js";
 import "./sliders/projects-slider.js";
+import "./sliders/reviews-slider.js";
 import "./sliders/sale-slider.js";
