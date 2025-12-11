765b82d5c9068c7975daceadb8bc72d53d2a2d39
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2025-12-11 04:59:05 +0400

    feat: implement Swiper sliders for panels with responsive styles
    - Added JavaScript functionality for Swiper sliders in panels, including autoplay and keyboard navigation.
    - Created styles for panels, tabs, main content, and sliders using SCSS with responsive design considerations.
    - Integrated intersection observer for autoplay control based on visibility.
    - Enhanced visual feedback for active thumbnails and added scrollbar functionality.

---
+++ src/js/libraries/libraries/swiper/sliders/panels-sliders.js
@@ -0,0 +1,88 @@
+import Swiper from "swiper";
+import { Autoplay, Keyboard, Scrollbar, Thumbs, } from "swiper/modules";
+
+/** @type {HTMLDivElement} */
+const panelsTabs = document.querySelector(".panels-tabs");
+/** @type {HTMLDivElement} */
+const panelsMain = document.querySelector(".panels-main");
+
+if (panelsTabs && panelsMain) {
+  const tabs = new Swiper(panelsTabs, {
+    breakpoints: {
+      "600.1": {
+        spaceBetween: 29,
+      },
+      "992.1": {
+        spaceBetween: 62,
+      },
+    },
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 24,
+    rewind: true,
+  });
+
+  const main = new Swiper(panelsMain, {
+    modules: [Keyboard, Thumbs],
+    keyboard: {
+      enabled: true,
+      pageUpDown: false,
+    },
+    thumbs: {
+      swiper: tabs,
+    },
+    spaceBetween: 48,
+    autoHeight: true,
+    rewind: true,
+  });
+}
+
+/** @type {NodeListOf<HTMLDivElement>} */
+const panelsContents = document.querySelectorAll(".panels-content");
+
+panelsContents.forEach(panelsContent => {
+  /** @type {HTMLDivElement} */
+  const panelsThumbs = panelsContent.querySelector(".panels-thumbs");
+  /** @type {HTMLDivElement} */
+  const panelsSlider = panelsContent.querySelector(".panels-slider");
+
+  if (panelsThumbs && panelsSlider) {
+    const scrollbar = panelsContent.querySelector(".panels-content__scrollbar");
+
+    const thumbs = new Swiper(panelsThumbs, {
+      slidesPerView: "auto",
+      spaceBetween: 12,
+      nested: true,
+    });
+
+    const slider = new Swiper(panelsSlider, {
+      modules: [Autoplay, Scrollbar, Thumbs],
+      autoplay: {
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+      scrollbar: {
+        draggable: true,
+        el: scrollbar,
+        enabled: true,
+      },
+      thumbs: {
+        swiper: thumbs,
+      },
+      loop: true,
+      spaceBetween: 12,
+      nested: true,
+      rewind: true,
+    });
+
+    const sliderIntersectionObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const { isIntersecting } = entry;
+
+        isIntersecting ? slider.autoplay.start() : slider.autoplay.stop();
+      });
+    });
+
+    sliderIntersectionObserver.observe(panelsSlider);
+  }
+});
--- src/js/libraries/libraries/swiper/swiper.js
+++ src/js/libraries/libraries/swiper/swiper.js
@@ -1,4 +1,5 @@
 import "./sliders/list-slider.js";
+import "./sliders/panels-sliders.js";
 import "./sliders/projects-slider.js";
 import "./sliders/reviews-slider.js";
 import "./sliders/sale-slider.js";
